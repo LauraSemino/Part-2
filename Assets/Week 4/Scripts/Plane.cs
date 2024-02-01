bd4f98af8b6749194de57e775fbb91ef6e9c0d91
Laura Semino <seminol@shernet.sheridancollege.ca>
2024-02-01 14:24:42 -0500

    Week4-Task2 Complete

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -9,13 +9,45 @@ public class Plane : MonoBehaviour
     public float newPointThreshold = 0.2f;
     Vector2 lastposition;
     LineRenderer lineRenderer;
+    Rigidbody2D rb2d;
+    Vector2 currentPosition;
+    public float speed = 1;
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
-    }
 
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if (points.Count > 0)
+        {
+            //rotate
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rb2d.rotation = -angle;
+        }
+        rb2d.MovePosition(rb2d.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+            if(Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+                for(int i = 0; i<lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
+    }
     private void OnMouseDown()
     {
         points = new List<Vector2>();
