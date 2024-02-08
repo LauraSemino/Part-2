a26460a4e0c7488b770d8dbccde952222c35112a
Laura Semino <seminol@shernet.sheridancollege.ca>
2024-02-08 15:28:29 -0500

    Week5-Task2 Complete

---
+++ Assets/Week 5/Scripts/HealthBar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    // Start is called before the first frame update
+    public void takedamage(float damage)
+    {
+        slider.value -= damage;
+    }
+}
--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -10,12 +10,16 @@ public class Knight : MonoBehaviour
     Rigidbody2D rb;
     Animator animator;
     bool clickingOnSelf = false;
+    public float health;
+    public float maxhealth = 5;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        health = maxhealth;
     }
     private void FixedUpdate()
     {
@@ -40,11 +44,25 @@ public class Knight : MonoBehaviour
     private void OnMouseDown()
     {
         clickingOnSelf = true;
-        animator.SetTrigger("TakeDamage");
-
+        SendMessage("takedamage",1);
+        
     }
     private void OnMouseUp()
     {
         clickingOnSelf = false;
+    }
+    public void takedamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxhealth);
+        if(health == 0)
+        {
+            animator.SetTrigger("Death");
+        }
+        else
+        {
+            animator.SetTrigger("TakeDamage");
+        }
+        
     }
 }
