c407401218d00d08e26a18c3105adecc26b8a583
Laura Semino <seminol@shernet.sheridancollege.ca>
2024-02-21 15:16:04 -0500

    Problem 2 Solved

--- Assets/Assignment/Scripts/Creature.cs
+++ Assets/Assignment/Scripts/Creature.cs
@@ -3,6 +3,8 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using UnityEngine.UIElements;
 
 public class Creature : MonoBehaviour
 {
@@ -11,15 +13,22 @@ public class Creature : MonoBehaviour
     public float speed = 4;
     Rigidbody2D rb;
     Animator animator;
+    Transform shrink;
+    public AnimationCurve ac;
     bool clicked = false;
     // 0 for red, 1 for green
     public float type;
+    public bool lerpDie = false;
+    public float lerpTimer = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        shrink = GetComponent<Transform>();
         destination = Camera.main.transform.position;
     }
     private void FixedUpdate()
@@ -38,24 +47,38 @@ public class Creature : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && clicked == true) 
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            animator.SetTrigger("move");
+            
             clicked = false;
         }
+        animator.SetFloat("move", move.magnitude);
+        
+        if (lerpDie == true)
+        {
+            //lerp time :D
+            lerpTimer += 1f * Time.deltaTime;
+            float interpolation = ac.Evaluate(lerpTimer);
+            shrink.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        }
+        
     }
     private void OnMouseUp()
     {
+        animator.SetTrigger("picked");
         clicked = true;
     }
     public void captured(float captured)
     {
-        //play death animation
+       
+
+        animator.SetTrigger("captured");
+        lerpDie = true;
 
         //check for colour
         if (captured == 0) 
         {
             if (type == 0)
             {
-                Destroy(gameObject);
+                Destroy(gameObject, 1);
             }
             if (type == 1)
             {
@@ -70,7 +93,7 @@ public class Creature : MonoBehaviour
             }
             if (type == 1)
             {
-                Destroy(gameObject);
+               Destroy(gameObject, 1);
             }
         }
 
