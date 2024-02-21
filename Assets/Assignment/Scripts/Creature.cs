515a7459feaf294d908d81edeb8bbda294583f6a
Laura Semino <seminol@shernet.sheridancollege.ca>
2024-02-21 12:36:48 -0500

    Problem 1 Solved, Creatures now can be moved and destroyed

--- Assets/Assignment/Scripts/Creature.cs
+++ Assets/Assignment/Scripts/Creature.cs
@@ -6,15 +6,70 @@ using UnityEngine;
 
 public class Creature : MonoBehaviour
 {
+    Vector2 destination;
+    Vector2 move;
+    public float speed = 4;
+    Rigidbody2D rb;
+    bool clicked = false;
+    // 0 for red, 1 for blue
+    public float type;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
+    private void FixedUpdate()
+    {
+        move = destination - (Vector2)transform.position;
+        if (move.magnitude < 0.1)
+        {
+            move = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + move.normalized * speed * Time.deltaTime);
 
+    }
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0) && clicked == true) 
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            clicked = false;
+        }
+    }
+    private void OnMouseUp()
+    {
+        clicked = true;
+    }
+    public void captured(float captured)
+    {
+        //play death animation
+
+        //check for colour
+        if (captured == 0) 
+        {
+            if (type == 0)
+            {
+                Destroy(gameObject);
+            }
+            if (type == 1)
+            {
+                //you lose
+            }
+        }
+        if (captured == 1)
+        {
+            if (type == 0)
+            {
+                //you lose
+            }
+            if (type == 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+
         
     }
 }
---
+++ Assets/Assignment/Scripts/Trigger.cs
@@ -0,0 +1,15 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    //Set 0 for red, 1 for blue
+    public float colour;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.SendMessage("captured", colour, SendMessageOptions.DontRequireReceiver);
+    }
+}
