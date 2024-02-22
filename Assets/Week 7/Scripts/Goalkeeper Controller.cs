f41f23689c2d5e349c9ea47d97c43c1e86946703
Laura Semino <seminol@shernet.sheridancollege.ca>
2024-02-22 17:16:47 -0500

    Week7-Task4b Complete

--- Assets/Week 7/Scripts/Goalkeeper Controller.cs
+++ Assets/Week 7/Scripts/Goalkeeper Controller.cs
@@ -3,12 +3,17 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class GoalkeeperController : MonoBehaviour
 {
     public Rigidbody2D rb;
     Vector2 direction;
     float distance;
+    Vector2 destination;
+    public float speed = 5;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,16 +28,19 @@ public class GoalkeeperController : MonoBehaviour
         direction = (Vector2)transform.position - (Vector2)Controller.SelectedPlayer.transform.position;
         distance = direction.magnitude;
         direction.Normalize(); 
-    }
+    }   
     private void FixedUpdate()
     {
         if(distance/2 < 2.5f)
         {
-            rb.position = (Vector2)transform.position - direction * distance / 2;
+            destination = (Vector2)transform.position - direction * distance / 2;
         }
         else
         {
-            rb.position = (Vector2)transform.position - direction * 2.5f;
+            destination = (Vector2)transform.position - direction * 2.5f;
         }
+
+        rb.position = Vector2.MoveTowards(rb.position, destination, speed * Time.deltaTime);
+           
     }
 }
